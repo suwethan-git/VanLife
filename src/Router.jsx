7f360c3eb48039e4149633b6b6fd8916bf3c8b45
Suwethan <suwethan8787@gmail.com>
2023-05-14 10:41:39 +0530

    Initial commit

---
+++ src/Router.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route, Link } from  "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About"
+import "./App.css";
+
+export default function Router() {
+    return (
+        <BrowserRouter>
+        <header>
+            <Link className="site-logo" to={"/"}>#VANLIFE</Link>
+            <nav>
+            <Link to={"/about"}>About</Link>
+            </nav>
+        </header>
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+        </Routes>
+        </BrowserRouter>
+    )
+}
